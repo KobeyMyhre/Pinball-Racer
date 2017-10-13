df9c592e175dd5a6f2095a3469f22b2eb7075457
Zomawia Sailo <s179140@students.aie.edu.au>
2017-10-13 16:07:06 -0700

    hotdog, seesaw,wrecking ball, minefield, and explosion prefabs

---
+++ Assets/ZOM LEVEL/LaunchOther.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchOther : MonoBehaviour {
+
+    public GameObject explosionGO;
+    ParticleSystem[] parts;
+    SphereCollider coll;
+    float timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+        explosionGO = Instantiate(explosionGO, transform);
+        parts = explosionGO.GetComponentsInChildren<ParticleSystem>();
+        coll = GetComponent<SphereCollider>();
+	}
+
+    void Explode()
+    {
+        for (int i = 0; i < parts.Length; ++i)
+        {
+            parts[i].Play();
+        }        
+        coll.enabled = false;
+    }    
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.GetComponentInParent<Rigidbody>().AddExplosionForce(2500f, transform.position, 10f, 5f, ForceMode.Impulse);
+        Explode();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (coll.enabled == false)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 10f)
+            {
+                coll.enabled = true;
+                timer = 0f;
+            }
+        }
+	}
+}
