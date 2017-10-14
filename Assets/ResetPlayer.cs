e0d5d968567b6f107ea7ec2a791d99555b088e89
Zomawia Sailo <s179140@students.aie.edu.au>
2017-10-13 17:52:03 -0700

    proto build. player reset, end game script

---
+++ Assets/ResetPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayer : MonoBehaviour {
+
+    Vector3 start;
+
+	// Use this for initialization
+	void Start () {
+        start = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.position = start;
+        }
+    }
+}
