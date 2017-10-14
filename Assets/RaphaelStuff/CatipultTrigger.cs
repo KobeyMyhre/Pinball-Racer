f195bb07ca553ef2808bf621a5e26e6869dd0989
Raphael Mastel III <s179135@students.aie.edu.au>
2017-10-13 17:54:19 -0700

    Yo Catipult

---
+++ Assets/RaphaelStuff/CatipultTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatipultTrigger : MonoBehaviour {
+
+    public GameObject Spring;
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col.gameObject.name);
+        if (Spring.GetComponent<CatipultEvent>().fired == false && Spring.GetComponent<CatipultEvent>().check == true)
+        {
+            Spring.GetComponent<CatipultEvent>().fired = true;
+            Spring.GetComponent<CatipultEvent>().check = false;
+        }
+    }
+}
