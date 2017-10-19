895f382e21908608011558361ef5f1a145b2d162
Zomawia Sailo <s179140@students.aie.edu.au>
2017-10-19 16:53:33 -0700

    proto2 build, more level work, fireworks flag juice

---
+++ Assets/ZOM LEVEL/PlayParticles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticles : MonoBehaviour {
+
+    public GameObject partsGO;
+    ParticleSystem[] parts;
+    BoxCollider coll;    
+
+    // Use this for initialization
+    void Start()
+    {
+        partsGO = Instantiate(partsGO, transform);
+        parts = partsGO.GetComponentsInChildren<ParticleSystem>();
+        coll = GetComponent<BoxCollider>();
+    }
+
+    void ParticlePlay()
+    {
+        for (int i = 0; i < parts.Length; ++i)
+        {
+            parts[i].Play();
+        }
+        coll.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            ParticlePlay();
+    }
+}
