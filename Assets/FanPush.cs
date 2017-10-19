0d39e4db1fdba27f8e922997a7083e4129ad25fc
Zomawia Sailo <s179140@students.aie.edu.au>
2017-10-18 17:07:09 -0700

    barrel objects, fan prefab

---
+++ Assets/FanPush.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanPush : MonoBehaviour {
+
+    public float windPower = 500f;
+
+    void OnTriggerStay(Collider other)
+    {
+        other.GetComponentInParent<Rigidbody>().AddForce(transform.forward * windPower, ForceMode.Impulse);
+    }
+}
