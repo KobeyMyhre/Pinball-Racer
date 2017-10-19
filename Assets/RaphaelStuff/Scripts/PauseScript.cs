dc0c6eade3256023a7ca448a45a49caf64582dba
Raphael Mastel III <s179135@students.aie.edu.au>
2017-10-18 17:16:12 -0700

    the game has a pause state.

---
+++ Assets/RaphaelStuff/Scripts/PauseScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+    public GameObject pauseIndicator;
+
+	// Use this for initialization
+	void Start () {
+        pauseIndicator.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.Escape))
+        {
+            if (!pauseIndicator.activeInHierarchy)
+            {
+                Pause();
+            }
+            else if(pauseIndicator.activeInHierarchy)
+            {
+                unPause();
+            }
+        }
+	}
+
+    private void Pause()
+    {
+        Time.timeScale = 0;
+        pauseIndicator.SetActive(true);
+    }
+    private void unPause()
+    {
+        Time.timeScale = 0;
+        pauseIndicator.SetActive(false);
+    }
+}
