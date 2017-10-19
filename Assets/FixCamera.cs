47a9fab149030bb0708d530bc319242495fcdf83
Raphael Mastel III <s179135@students.aie.edu.au>
2017-10-19 15:00:49 -0700

    merge master->raphael

---
+++ Assets/FixCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<CarController>().CheckGround = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<CarController>().CheckGround = true;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
